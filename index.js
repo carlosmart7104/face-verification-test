7cd0f564c63359c506ecc6fe199ea4b1a2ffac5b
Carlos Martínez <carlosmart7104@gmail.com>
2019-04-17 01:39:50 -0500

    Allow CORS

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api', apiRouter);
 
 app.listen(process.env.PORT || 3000, function () {
